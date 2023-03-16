903c700ccac10e0b5ed92ba1c9f2f94c5b9a44a4
Abdullah Muhammad Siam <abdullahms838@gmail.com>
2023-03-17 01:20:21 +0600

    last comment

--- js/login.js
+++ js/login.js
@@ -2,7 +2,7 @@ document.getElementById('btn-submit').addEventListener('click', function () {
     const emailField = document.getElementById('email-field').value;
     const passwordField = document.getElementById('password-field').value;
 
-    if (emailField === 'sontan@baap.com' && passwordField === 'secret') {
+    if (emailField === 'bank@abbu.com' && passwordField === 'secret') {
         window.location.href = 'bank.html';
     }
     else {
